2eb339111dbd19c413e6c9957bfa9638c47d951a
simrannadaf2002 <nsimran026@gmail.com>
2024-09-07 14:46:55 +0530

    added auth for user for sign in and log in, chat feature

---
+++ lib/screens/auth.dart
@@ -0,0 +1,205 @@
+import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart' show ByteData, Uint8List, rootBundle;
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() {
+    return _AuthScreenState();
+  }
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _firebase = FirebaseAuth.instance;
+  final _form = GlobalKey<FormState>();
+  var _isLogin = false;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+  var _enteredUsername = '';
+  File? _selectedImage;
+  var _isAuthenticating = false;
+
+  void _submit() async {
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Please Fill all details and upload profile photo.')));
+      return;
+    }
+    if (isValid) {
+      _form.currentState!.save();
+      try {
+        setState(() {
+          _isAuthenticating = true;
+        });
+        if (_isLogin) {
+          final userCredential = await _firebase.signInWithEmailAndPassword(
+              email: _enteredEmail, password: _enteredPassword);
+          print(userCredential);
+        } else {
+          final userCredential = await _firebase.createUserWithEmailAndPassword(
+              email: _enteredEmail, password: _enteredPassword);
+
+          final storageRef = FirebaseStorage.instance
+              .ref()
+              .child('user_images')
+              .child('${userCredential.user!.uid}.jpg');
+
+          if (_selectedImage == null) {
+            ByteData data = await rootBundle.load('assets/user.png');
+            List<int> bytes = data.buffer.asUint8List();
+            await storageRef.putData(Uint8List.fromList(bytes));
+          } else {
+            await storageRef.putFile(_selectedImage!);
+          }
+          final imageUrl = await storageRef.getDownloadURL();
+          FirebaseFirestore.instance
+              .collection('users')
+              .doc(userCredential.user!.uid)
+              .set({
+            'username': _enteredUsername,
+            'email': _enteredEmail,
+            'image_url': imageUrl,
+          });
+        }
+      } on FirebaseAuthException catch (error) {
+        if (error.code == 'email-already-in-use') {
+          //
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content:
+                Text(error.message ?? 'Authentication Credential Fails!')));
+        setState(() {
+          _isAuthenticating = false;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                  top: _isLogin ? 160 : 80, left: 20, right: 20, bottom: 20),
+              width: 120,
+              child: Image.asset('assets/chat.png'),
+            ),
+            Card(
+              margin: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Form(
+                    key: _form,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        if (!_isLogin)
+                          UserImagePicker(
+                            onPickedImage: (pickedImage) {
+                              _selectedImage = pickedImage;
+                            },
+                          ),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Email Address'),
+                          keyboardType: TextInputType.emailAddress,
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
+                          validator: (value) {
+                            if (value == null ||
+                                value.trim().isEmpty ||
+                                !value.contains('@')) {
+                              return 'Plaese enter valid email address';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _enteredEmail = value!;
+                          },
+                        ),
+                        if (!_isLogin)
+                          TextFormField(
+                            validator: (value) {
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  value.trim().length < 4) {
+                                return 'Plaese enter at least 4 characters username';
+                              }
+                              return null;
+                            },
+                            enableSuggestions: false,
+                            decoration:
+                                const InputDecoration(labelText: 'Username'),
+                            onSaved: (value) {
+                              _enteredUsername = value!;
+                            },
+                          ),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Password'),
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.trim().length < 6) {
+                              return 'Password must be at least 6 characters long';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _enteredPassword = value!;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        if (_isAuthenticating)
+                          const Padding(
+                            padding: EdgeInsets.all(20.0),
+                            child: CircularProgressIndicator(),
+                          ),
+                        if (!_isAuthenticating)
+                          ElevatedButton(
+                            onPressed: _submit,
+                            child: Text(_isLogin ? "Login" : "Signup"),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                            ),
+                          ),
+                        if (!_isAuthenticating)
+                          TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Text(_isLogin
+                                  ? "Create an account"
+                                  : "I already have an account!")),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
